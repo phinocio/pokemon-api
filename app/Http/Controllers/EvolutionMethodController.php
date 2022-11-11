ea06610fbc9e563dd124234db1ee49e469a51087
Phinocio <github@phinocio.com>
2022-11-11 13:21:13 -0700

    progress

---
+++ app/Http/Controllers/EvolutionMethodController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\EvolutionMethod;
+use Illuminate\Http\Request;
+
+class EvolutionMethodController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\EvolutionMethod  $evolutionMethod
+     * @return \Illuminate\Http\Response
+     */
+    public function show(EvolutionMethod $evolutionMethod)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\EvolutionMethod  $evolutionMethod
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(EvolutionMethod $evolutionMethod)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\EvolutionMethod  $evolutionMethod
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, EvolutionMethod $evolutionMethod)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\EvolutionMethod  $evolutionMethod
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(EvolutionMethod $evolutionMethod)
+    {
+        //
+    }
+}
